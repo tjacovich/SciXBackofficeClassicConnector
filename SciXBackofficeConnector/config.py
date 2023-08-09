ede9b344f77c774b13e62391fadf0fb437a4a2d0
tjacovich <tjacovich@cfa.harvard.edu>
2023-08-09 13:18:45 -0400

    bump setuptools to make it compatible with confluent-connect container.

--- SciXBackofficeConnector/config.py
+++ SciXBackofficeConnector/config.py
@@ -3,7 +3,7 @@
 LOGGING_LEVEL = "INFO"
 LOG_STDOUT = True
 # SQLALCHEMY Configuration
-SQLALCHEMY_URL = "postgresql://parser:parser@localhost:5432/parser"
+SQLALCHEMY_URL = "postgresql://postgres:postgres@postgres:5432/master"
 SQLALCHEMY_ECHO = False
 # REDIS Configuration
 REDIS_HOST = "localhost"
@@ -11,24 +11,6 @@ REDIS_PORT = 6379
 # Kafka Configuration
 KAFKA_BROKER = "kafka:9092"
 SCHEMA_REGISTRY_URL = "http://schema-registry:8081"
-# PARSER AVRO Schema Parameters
-PARSER_INPUT_SCHEMA = "ParserInputSchema"
-PARSER_INPUT_TOPIC = "ParserInput"
-PARSER_OUTPUT_SCHEMA = "ParserOutputSchema"
-PARSER_OUTPUT_TOPIC = "ParserOutput"
+# Connector AVRO Schema Parameters
 HARVESTER_OUTPUT_SCHEMA = "HarvesterOutputSchema"
 HARVESTER_OUTPUT_TOPIC = "HarvesterOutput"
-# S3 Configuration
-S3_PROVIDERS = ["AWS", "MINIO"]
-# AWS Configuration
-AWS_ACCESS_KEY_ID = "CHANGEME"
-AWS_SECRET_ACCESS_KEY = "SECRETS"
-AWS_DEFAULT_REGION = "us-east-1"
-PROFILE_NAME = "SESSION_PROFILE"
-AWS_BUCKET_NAME = "BUCKETNAME"
-AWS_BUCKET_ARN = "BUCKETARN"
-# MINIO Configuration
-MINIO_ACCESS_KEY_ID = "admin"
-MINIO_SECRET_ACCESS_KEY = "supersecret"
-MINIO_BUCKET_NAME = "scix-parser"
-MINIO_S3_URL = "http://minio:9000"
