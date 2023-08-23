ce3a20c331a0fc70722a1dc9b0e105e8861e97a9
tjacovich <tjacovich@cfa.harvard.edu>
2023-08-23 17:14:12 -0400

    Updated config files. Added AVRO schema for pulling from master database.

--- SciXBackofficeConnector/scripts/start_connector.py
+++ SciXBackofficeConnector/scripts/start_connector.py
@@ -3,7 +3,14 @@ import json
 import os
 
 request_header = {"Accept": "application/json", "Content-Type": "application/json"}
-connector_config_path = ""
-connector_data = json.load(connector_config_path)
+proj_home = os.path.abspath(os.path.join(__file__ ,"../.."))
+connector_config_file=os.path.join(proj_home, "connect_config_files/register-postgres.json")
+with open(connector_config_file) as f:
+    connector_data = json.load(f)
 
-requests.post(url=os.getenv['CONNECTOR_URL'], headers=request_header, data=connector_data)
+resp = requests.post(\
+    url=os.getenv("CONNECT_URL", "http://localhost:8083/connectors/"), \
+    headers=request_header, \
+    data=json.dumps(connector_data))
+
+print(json.dumps(resp.json()))
